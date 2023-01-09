43b46b2c406fe6f37a129926307f0db90857632d
KorolevAV <antoshka.992@mail.ru>
2023-01-09 03:06:58 +0300

    add test_product_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,12 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CLASS_NAME, "btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > h1")
+    PRICE_PRODUCT = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color")
+    # SUCCESFULL_MESSAGE_ADDED_TO_BASKET = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div")
+    PRODUCT_NAME_IN_MESSAGE_ADDED_TO_BASKET = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    # MESSAGE_BASKET_TOTAL_PRICE = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1)")
+    BASKET_TOTAL_PRICE = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
---
+++ pages/product_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+
+    def should_be_add_to_basket(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET)
+
+    def add_to_basket_product(self):
+        self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET).click()
+    
+    def check_product_name(self):
+        assert self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text == \
+            self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_MESSAGE_ADDED_TO_BASKET).text, \
+                "Products' name are not the same"
+    
+    def check_price_product(self):
+        assert self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT).text == \
+            self.browser.find_element(*ProductPageLocators.BASKET_TOTAL_PRICE).text, \
+                "Product's price does't match"
